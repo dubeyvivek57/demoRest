89e2fa18be5d4e8f38b203bbbb383bf58bc4e729
Vivek Dubey <dubey.ssiet@hotmail.com>
2018-12-25 22:01:17 +0530

    Crete First Controller

---
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @GetMapping("/{name}")
+    fun getName(@PathVariable("name") name: String): String {
+        return "Name : $name \n"
+    }
+}
